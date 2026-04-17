946cae0022530f7342e660ba236839eb5e14d372
COLOMAR <124306888+colomarr@users.noreply.github.com>
2026-04-17 17:59:53 -0600

    determinize completed, powerset construction

--- lib/automata.ex
+++ lib/automata.ex
@@ -5,7 +5,7 @@ defmodule Automata do
       :states => [0,1,2,3],
       :alphabet => [:a, :b],
       :transitions => %{
-        {0, :a} => [0,1], #key es estado + symbol -> resultado
+        {0, :a} => [0,1],
         {0, :b} => [0],
         {1, :b} => [2],
         {2, :b} => [3]
@@ -15,37 +15,57 @@ defmodule Automata do
     }
   end
 
-  def determinize(nfa) do
+def determinize(nfa) do
+  start_dfa = MapSet.new([nfa.start])
+  pending = [start_dfa]
+  discovered_states = [start_dfa]
+  transitions_dfa = %{}
 
-    start_dfa = MapSet.new([nfa.start])
-    pending = [start_dfa]
-    discovered_states = [start_dfa]
-    transitions_dfa = %{}
+  {all_states, all_transitions} = loop(nfa, pending, discovered_states, transitions_dfa)
 
+  accept_dfa =
+    Enum.filter(all_states, fn state_set ->
+      Enum.any(nfa.accept, fn acc -> MapSet.member?(state_set, acc) end)
+    end)
+
+  %{
+    states: all_states,
+    alphabet: nfa.alphabet,
+    transitions: all_transitions,
+    start: start_dfa,
+    accept: accept_dfa
+  }
+end
+
+  def loop(_nfa, [], discovered_states, transitions_dfa) do
+    {discovered_states, transitions_dfa}
   end
-
   def loop(nfa, [current|rest], discovered_states, transitions_dfa) do
-    #el loopsito pa procesar de que cada estado hasta que no salgan mas nuevos y pues guardar transiciones pipipipi
-    if pending == [] do
-      regresar result
-    end
-  else
-    [head | tail] = pending
-    current_state = head
-    new_state = transition_func(nfa, current_state, alphabet_symbol)
+    {new_discovered, new_pending, new_transitions} =
+      Enum.reduce(nfa.alphabet, {discovered_states, rest, transitions_dfa}, fn symbol, {disc, pend, trans} ->
+        new_state = transition_func(nfa, current, symbol)
+        trans = Map.put(trans, {current, symbol}, new_state)
+        if not Enum.member?(disc, new_state) do
+          {[new_state | disc], [new_state | pend], trans}
+        else
+          {disc, pend, trans}
+        end
+      end)
 
+    loop(nfa, new_pending, new_discovered, new_transitions)
   end
-  #nfa = Automata.nfa()
-  #Automata.determinize(nfa)
-
 
   def transition_func(nfa, states, alphabet_symbol) do
     trans_list =
-      Enum.map(states, fn x -> Map.get(nfa.transitions, {x, alphabet_symbol}, []) end) #para recorrer cada estado
+      Enum.map(states, fn x -> Map.get(nfa.transitions, {x, alphabet_symbol}, []) end)
 
-    final_list = List.flatten(trans_list) #lista bomnita
-    MapSet.new(final_list) # dfa
+    final_list = List.flatten(trans_list)
+    MapSet.new(final_list)
 
   end
 
 end
+
+#nfa = Automata.nfa()
+#dfa = Automata.determinize(nfa)
+#IO.inspect(dfa)
